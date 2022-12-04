3c7e9e86f978c7043966d72997ed7e190d528258
B-Phuong <nguyennhubaophuong2008@gmail.com>
2022-12-04 18:03:51 +0700

    Initial files

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useContext } from "react";
+import { StoreContext } from "./Redux/Store/Store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { API_USER } from "./common/ApiController";
+import PrivateAdminRoutes from "./utils/PrivateAdminRoutes";
+import HeaderAdmin from "./Page/Header/HeaderAdmin";
+import SignIn from "./Components/SignIn/SignIn";
+import AdminMenu from "./Page/AdminMenu";
+import AdminMovies from "./Page/AdminMovies";
+import AdminFoodDrinks from "./Page/AdminFoodsDrinks";
+import Header from "./Components/Header/HeaderAdmin";
+
+function App() {
+  const store = useContext(StoreContext);
+  useEffect(() => {
+    store.accounts.AccountDispatch({
+      type: "SIGN_IN",
+      payload: localStorage.getItem("taiKhoan"),
+    });
+  }, [localStorage.getItem("taiKhoan")]);
+
+  useEffect(() => {
+    if (store.accounts.userAccount.account) {
+      store.accounts.AccountDispatch({
+        type: "SIGN_IN",
+        payload: localStorage.getItem("taiKhoan"),
+      });
+      // let token = JSON.parse(localStorage.getItem("token"));
+      // fetch(API_USER.PROFILE, {
+      //   headers: {
+      //     //Nó sẽ nói cho sever biết, web này sẽ gởi giá trị đi là json
+      //     "Content-Type": "application/json",
+      //     Authorization: `Basic ${token}`,
+      //   },
+      //   method: "GET",
+      // })
+      //   .then((res) => res.json())
+      //   .then((dt) =>
+      //     store.accounts.AccountDispatch({
+      //       type: "SIGN_IN",
+      //       payload: dt.data,
+      //     })
+      //   );
+    }
+  }, [store.accounts.userAccount.account]);
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <Header /> */}
+
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          {/* <Route path="/" element={<Home />} /> */}
+
+          <Route element={<PrivateAdminRoutes />}>
+            <Route path="/Admin/*" element={<AdminMenu />} />
+          </Route>
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/ApiController.js
@@ -0,0 +1,54 @@
+export const API_DOMAIN = "http://localhost:5000/";
+
+export const API_MOVIE = {
+  COMING: API_DOMAIN + "movies/coming",
+  SHOWING: API_DOMAIN + "movies",
+  DETAIL: API_DOMAIN + `movies/`,
+  UPDATE: API_DOMAIN + `admins/movie/`,
+  ADD: API_DOMAIN + `admins/movie`,
+};
+
+export const API_SHOWTIMES = {
+  ADD: API_DOMAIN + `admins/movie/`,
+  DELETE: API_DOMAIN + "admins/movie/",
+};
+export const API_ROOMS = {
+  GET: API_DOMAIN + "admins/movie/room",
+};
+
+export const API_THEATERS = {
+  SHOWING: API_DOMAIN + "movies/showing",
+  THEATERS: API_DOMAIN + "movies/movietheater",
+};
+
+export const API_SHOWTIME = {
+  CLUSTER: API_DOMAIN + "movies/cluster/",
+};
+
+export const API_USER = {
+  PROFILE: API_DOMAIN + "users",
+  GET_ALL: API_DOMAIN + "admins/user",
+  GET_DETAIL_ADMIN: API_DOMAIN + "admins",
+  CHANGE_PASSWORD: API_DOMAIN + "users/editPassword",
+  HISTORY_TICKET: API_DOMAIN + "users/history",
+};
+
+export const API_ACCOUNTS = {
+  SIGNIN: API_DOMAIN + "accounts/signin",
+  SIGNUP: API_DOMAIN + "accounts/signUp",
+};
+export const API_FOODDRINKS = {
+  GET_ALL: API_DOMAIN + "users/food_drink/",
+  DETAIL: API_DOMAIN + "users/food_drink/",
+  UPDATE: API_DOMAIN + "admins/food_drink/",
+  ADD: API_DOMAIN + "admins/food_drink",
+};
+
+export const API_BOOKING = {
+  BOOK_TICKET: API_DOMAIN + "users/",
+  SEND_EMAIL: API_DOMAIN + "users/sendEmailBooking",
+};
+export const API_CHARTS = {
+  GETREVENUE: API_DOMAIN + "admins/revenue",
+  GETTICKETS: API_DOMAIN + "admins/ticketBookings",
+};
